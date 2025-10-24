c00faae5c56254243b608d591ac0765a48527317
richardkayovidaldarosa <richard.vidal.rosa@escola.pr.gov.br>
2025-10-24 14:43:10 +0000

    save

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function Acender(){
-    document.getElementById("Lampada").src="on.png"
-    console.log("On")
+const lampada = document.getElementById("lampada");
+
+lampada.addEventListener("mouseover",acender);
+lampada.addEventListener("mouseout",apagar);
+
+function acender(){
+    lampada.src = "on.png";
+    document.body.style.background = "#ffeb3b22"
 }
 
-function Apagar(){
-    document.getElementById("Lampada").src="off.png"
-    console.log("Off")
+function apagar(){
+    lampada.src = "off.png";
+    document.body.style.background = "#222"
 }
